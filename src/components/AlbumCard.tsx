637ce0b3b66261abb3b388ec3e6bc1447e087f99
AndyCotruta <acotruta18@gmail.com>
2022-12-09 16:57:57 +0100

    first cards are in the house

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -1,5 +1,29 @@
-const AlbumCard = () => {
-  return <div>This is a card</div>;
+import { MainAlbum } from "../redux/types/Album";
+import { BsPlayCircleFill } from "react-icons/bs";
+
+interface AlbumProps {
+  albumData: MainAlbum;
+}
+
+const AlbumCard = ({ albumData }: AlbumProps) => {
+  return (
+    <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+      <div className="recently-played-cards card mb-3">
+        <img
+          src={albumData.album.cover}
+          className="card-img-top"
+          alt="album-cover"
+        ></img>
+        <div className="recently-played-card-body card-body">
+          <p className="card-text p-cards-title">{albumData.album.title}</p>
+          <p className="card-text">{albumData.artist.name}</p>
+        </div>
+        <button className="green-player-btn-rp">
+          <BsPlayCircleFill />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default AlbumCard;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,7 @@ import { HomeType } from "../redux/types/HomeType";
 import { fetchAlbumAction } from "../redux/actions/actions";
 import { Row } from "react-bootstrap";
 import AlbumCard from "./AlbumCard";
+import { MainAlbum } from "../redux/types/Album";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -26,11 +27,52 @@ const Home = () => {
 
   return (
     <div>
-      <h1>This is the homepage</h1>
+      <div className="center-section">
+        <div className="center-nav-bar px-35 py-2">
+          <div className="btns-prev-next">
+            <img className="img-btn" src="img/prev.svg" alt="" />
 
-      {goodMorning.map((album) => {
-        <AlbumCard key={album.id} />;
-      })}
+            <img className="img-btn" src="img/next.svg" alt="" />
+          </div>
+
+          <div className="user-name-div d-flex text-white">
+            <div className="user-name-image px-1">
+              <i className="bi bi-person-circle"></i>
+            </div>
+            <div className="user-name-text px-1">User Name</div>
+            <div className="px-1">
+              <i className="dropdownBtn bi bi-caret-down-fill"></i>
+            </div>
+          </div>
+          <button className="btn d-none navbar-signup-btn">Sign Up</button>
+          <button className="btn d-none navbar-login-btn">Log In</button>
+          <ul className="dropMenu d-none">
+            <li>Account</li>
+            <li>Profile</li>
+            <li>Upgrade to Premium</li>
+            <li>Settings</li>
+            <li className="logout-btn">Log out</li>
+          </ul>
+        </div>
+
+        <div className="good-morning-section">
+          <h2 className="px-35">Good Morning</h2>
+          <div className="good-morning row mx-1">
+            {" "}
+            {goodMorning.slice(0, 6).map((album: MainAlbum) => (
+              <AlbumCard albumData={album} key={album.id} />
+            ))}
+          </div>
+        </div>
+        <div className="recently-played-section">
+          <h2 className="px-35">Recently Played</h2>
+          <div className="recently-played row mx-1"></div>
+        </div>
+        <div className="shows-to-try-section">
+          <h2 className="px-35">Shows To Try</h2>
+          <div className="shows-to-try row mx-1"></div>
+        </div>
+      </div>
     </div>
   );
 };
