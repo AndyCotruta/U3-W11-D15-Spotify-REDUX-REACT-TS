6609bfe14b5385269da4f9f3aa8d3c962e90fc9a
AndyCotruta <acotruta18@gmail.com>
2022-12-13 17:24:39 +0100

    some styling

--- src/components/TracksLi.tsx
+++ src/components/TracksLi.tsx
@@ -7,9 +7,23 @@ interface tracksProps {
 
 const TracksLi = ({ trackData, index }: tracksProps) => {
   return (
-    <li>
-      {index + 1}.{trackData.title}
-      {Math.floor(trackData.duration / 60)}:{trackData.duration % 60}
+    <li className="track-info d-flex justify-content-between">
+      <span className="mr-3 tracks-index">{index + 1}</span>
+      <div className="tracks-section d-flex align-items-center">
+        {trackData.title}
+      </div>
+      <div className="tracks-section text-center d-flex align-items-center justify-content-center">
+        {" "}
+        {trackData.artist.name}
+      </div>
+      <div className="tracks-section d-flex align-items-center">
+        {" "}
+        {trackData.explicit_lyrics}
+      </div>
+
+      <div className="tracks-section  d-flex align-items-center justify-content-end px-1">
+        {Math.floor(trackData.duration / 60)}:{trackData.duration % 60}
+      </div>
     </li>
   );
 };
