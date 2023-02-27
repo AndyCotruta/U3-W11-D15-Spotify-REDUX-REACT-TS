2ad9ceac9e2e0ccfc262fe421cc3c51ffa4bfb25
AndyCotruta <acotruta18@gmail.com>
2023-02-27 12:21:37 +0100

    more work on your Library

--- src/components/YourLibrary.tsx
+++ src/components/YourLibrary.tsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
+import { MainAlbum } from "../redux/types/Album";
 import BannerNav from "./BannerNav";
 
 const YourLibrary = () => {
   const [playlists, setPlaylists] = useState("");
   const selectedTab = useSelector((state: RootState) => state.yourLibrary.tab);
+  const likedAlbums = useSelector(
+    (state: RootState) => state.yourLibrary.albums
+  );
 
   return (
     <div className="center-section text-white">
       <BannerNav />
-      <div className="d-flex mt-5">
+      <div className="d-flex flex-column mt-5">
         <h1>{selectedTab}</h1>
+        <div>
+          {selectedTab === "Albums" &&
+            likedAlbums.map((album: MainAlbum) => (
+              <div>We have liked albums</div>
+            ))}
+        </div>
       </div>
     </div>
   );
