a4c26413e83f216c39bb5f8e6a64800d14affce4
AndyCotruta <acotruta18@gmail.com>
2023-03-12 11:58:55 +0100

    dont know

--- src/components/hooks/useAverageColor.tsx
+++ src/components/hooks/useAverageColor.tsx
@@ -1,60 +1,64 @@
-import { useMemo } from 'react'
+import { useMemo } from "react";
 
 interface AverageColor {
-    red: number;
-    green: number;
-    blue: number;
+  red: number;
+  green: number;
+  blue: number;
 }
 
 function useAverageColor(imageElement: string): AverageColor | null {
+  const averageColor = useMemo(() => {
+    if (!imageElement) return null;
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    img.src = imageElement;
 
-    const averageColor = useMemo(() => {
-        if (!imageElement) return null
-        const img = new Image();
-        img.crossOrigin = "anonymous";
-        img.src = imageElement;
+    // await new Promise((resolve) => {
+    //     img.onload = resolve;
+    // });
 
-        // await new Promise((resolve) => {
-        //     img.onload = resolve;
-        // });
+    const canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
 
-        const canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      throw new Error("Unable to get context");
+    }
 
-        const ctx = canvas.getContext("2d");
-        if (!ctx) {
-            throw new Error("Unable to get context");
-        }
+    ctx.drawImage(img, 0, 0);
 
-        ctx.drawImage(img, 0, 0);
+    const imageData = ctx.getImageData(
+      0,
+      0,
+      canvas.width || 100,
+      canvas.height || 100
+    );
+    const data = imageData.data;
 
-        const imageData = ctx.getImageData(0, 0, canvas.width || 100, canvas.height || 100);
-        const data = imageData.data;
+    let red = 0;
+    let green = 0;
+    let blue = 0;
 
-        let red = 0;
-        let green = 0;
-        let blue = 0;
+    for (let i = 0; i < data.length; i += 4) {
+      red += data[i];
+      green += data[i + 1];
+      blue += data[i + 2];
+    }
 
-        for (let i = 0; i < data.length; i += 4) {
-            red += data[i];
-            green += data[i + 1];
-            blue += data[i + 2];
-        }
+    const pixels = data.length / 4;
+    const averageRed = Math.round(red / pixels);
+    const averageGreen = Math.round(green / pixels);
+    const averageBlue = Math.round(blue / pixels);
 
-        const pixels = data.length / 4;
-        const averageRed = Math.round(red / pixels);
-        const averageGreen = Math.round(green / pixels);
-        const averageBlue = Math.round(blue / pixels);
+    return {
+      red: averageRed,
+      green: averageGreen,
+      blue: averageBlue,
+    };
+  }, [imageElement]);
 
-        return {
-            red: averageRed,
-            green: averageGreen,
-            blue: averageBlue,
-        }
-    }, [imageElement])
-
-    return averageColor;
+  return averageColor;
 }
 
-export default useAverageColor
+export default useAverageColor;
